1aee578b607382c787f7a763db40e26fc1cd3721
Jose Lopez <josel786@gmail.com>
2016-05-10 19:15:27 -0400

    pdatweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
         </figure>
         <div class="soundcloud-embed">
             <iframe width="100%" height="300" scrolling="no" frameborder="no"
-                    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/201154586&amp;auto_play=false&amp;hide_related=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true&amp;visual=true"></iframe>
+                    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/115820844&amp;auto_play=false&amp;hide_related=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true&amp;visual=true"></iframe>
         </div>
     </div>
     <footer class="site-footer">
