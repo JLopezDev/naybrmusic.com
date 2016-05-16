19890f99f66c6e03cc1c88daa4172e49bcf20f10
Jose Lopez <josel@up3.com>
2016-05-16 10:16:05 -0400

    tweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,7 +67,7 @@
                 </div>
             </div>
         </section>
-        <section class="fold fold--dark" , style="background-color: #0f0f0f;">
+        <section class="" , style="background-color: #0f0f0f;">
             <div class="soundcloud__embed">
                 <iframe width="100%" height="340" scrolling="yes" frameborder="no"
                         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/115820844&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
